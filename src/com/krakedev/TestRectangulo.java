16aedaabf87ec603d435f997151cf34d549a010a
juandiegojd252 <juandiegojd252@gmail.com>
2023-12-10 19:47:22 -0500

    Constructor en la clase Rectangulo y modificacion en TestRectangulo

--- src/com/krakedev/TestRectangulo.java
+++ src/com/krakedev/TestRectangulo.java
@@ -2,14 +2,8 @@ package com.krakedev;
 
 public class TestRectangulo {
 	public static void main(String[] args) {
-		Rectangulo rec1 = new Rectangulo();
-		Rectangulo rec2 = new Rectangulo();
-
-		rec1.setAltura(4);
-		rec1.setBase(2);
-
-		rec2.setBase(6);
-		rec2.setAltura(3);
+		Rectangulo rec1 = new Rectangulo(4, 2);
+		Rectangulo rec2 = new Rectangulo(6, 3);
 
 		System.out.println("El perimetro del rectangulo 1 es:" + rec1.calcularPerimetro());
 		System.out.println("El perimetro del rectangulo 2 es:" + rec2.calcularPerimetro());
