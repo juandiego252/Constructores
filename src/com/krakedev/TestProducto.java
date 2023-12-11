5b14882f9e9e78b8cba4a0ed231a17f0e61df02e
juandiegojd252 <juandiegojd252@gmail.com>
2023-12-10 19:44:58 -0500

    Constructor en la clase Productos y modificacion TestProducto

--- src/com/krakedev/TestProducto.java
+++ src/com/krakedev/TestProducto.java
@@ -3,45 +3,24 @@ package com.krakedev;
 public class TestProducto {
 
 	public static void main(String[] args) {
-		
-		
-		Producto productoA;
-		Producto productoB; 
-		Producto productoC;
-		
-		productoA = new Producto();
-		productoB = new Producto();
-		productoC = new Producto();
-		
-		productoA.setNombre("Clotrimazol");
-		productoA.setDescripcion("Medicamento antifungico");
-		productoA.setPrecio(19.99);
-		productoA.setStockActual(30);
-		
-		productoB.setNombre("Aspirina");;
-		productoB.setDescripcion("Medicamento antiinflamatorio");
-		productoB.setPrecio(8.75);
-		productoB.setStockActual(120);
-		
-		productoC.setNombre("Ibuprofeno");
-		productoC.setDescripcion("Medicamento antiinflamatorio");
-		productoC.setPrecio(5.75);
-		productoC.setStockActual(300);
-		
-		
+
+		Producto productoA = new Producto("Clotrimazol", "Medicamento antifungico", 19.99, 30);
+		Producto productoB = new Producto("Aspirina", "Medicamento antiinflamatorio", 8.75, 120);
+		Producto productoC = new Producto("Ibuprofeno", "Medicamento antiinflamatorio", 5.75, 300);
+
 		System.out.println("---------------");
 		System.out.println("Nombre:" + productoA.getNombre());
 		System.out.println("Descripcion:" + productoA.getDescripcion());
 		System.out.println("Precio $:" + productoA.getPrecio());
 		System.out.println("Stock Actual:" + productoA.getStockActual());
-		
+
 		System.out.println("---------------");
 
 		System.out.println("Nombre:" + productoB.getNombre());
 		System.out.println("Descripcion:" + productoB.getDescripcion());
 		System.out.println("Precio $:" + productoB.getPrecio());
 		System.out.println("Stock Actual:" + productoB.getStockActual());
-		
+
 		System.out.println("---------------");
 		System.out.println("Nombre:" + productoC.getNombre());
 		System.out.println("Descripcion:" + productoC.getDescripcion());
